5ce0ed48c1c76c329e3c9d7de580e8b596441e8f
Kevin <kevingm31@gmail.com>
2019-05-11 13:42:32 -0500

    Add user model and db connect

--- server/index.js
+++ server/index.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 //Initialize new instance of Express
 const app = express();
 
-app.use(bodyParser.json());
-app.use(cors());
+//Require Database
+const db = require('../config/keys').MongoURI;
+
+// Establish Connection
+mongoose.connect(db, {useNewUrlParser: true})
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 //Require routes
-const routines = require('./routes/api/routines')
+const routines = require('./routes/api/routines');
+const users = require('./routes/api/users');
 
-app.use('/api/routines', routines)
+app.use('/api/routines', routines);
+app.use('/api/users', users);
 
+// BodyParser
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 
 
---
+++ server/routes/api/users.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../../../models/users');
+
+router.post('/users', (req, res) => {
+
+})
+
+
+module.exports = router;
+
