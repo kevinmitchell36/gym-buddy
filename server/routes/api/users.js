7a04e56199f277b1f7731098379609cba182d9d9
Kevin <kevingm31@gmail.com>
2019-05-16 13:12:31 -0500

    Add bcrypt to user POST action

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../../../models/user');
+const bcrypt = require('bcryptjs');
 
 router.get('/', (req, res) => {
   User.find({}).then((users) => {
@@ -9,9 +10,34 @@ router.get('/', (req, res) => {
 });
 
 router.post('/signup', (req, res) => {
-  User.create(req.body).then((users) => {
-    res.send(users);
-  });
+  const {name, username, email, password} = req.body;
+  let errors = []
+
+  if( !name || !username || !email || !password) {
+    errors.push("All fields are required");
+  }
+
+  if(errors.length > 0) {
+    console.log(errors);
+  } else {
+    //Generates new user without saving
+    const newUser = new User({
+      name,
+      username,
+      email,
+      password
+    });
+    bcrypt.genSalt(10, (err, salt) => 
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if(err) throw err;
+        newUser.password = hash;
+        newUser.save()
+        .then(users => {
+          res.send(users)
+        })
+        .catch(err => console.log(err));
+    }))
+  }
 });
 
 
